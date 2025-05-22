aefa2d2f488b3109ba31a6d56f77db1bf1dc31ed
Abner David Dotel Chalas <dotelchalas@gmail.com>
2025-05-22 15:37:28 -0400

    Actualizacion

--- SIGESAEntidades/EstadoReserva.cs
+++ SIGESAEntidades/EstadoReserva.cs
@@ -4,9 +4,7 @@ namespace SigesaEntidades
     public class EstadoReserva
     {
         public int IdEstado {  get; set; } 
-
         public string Nombre { get; set; }  = null!;
-
         public string FechaCreacion { get; set; } = null!;
     }
 }
