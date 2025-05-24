3bfdd87f13d09b0f2fd901048ef785d1ba55fee1
Abner David Dotel Chalas <dotelchalas@gmail.com>
2025-05-24 14:43:10 -0400

    Creacion de dos nuevas entidades, con fines de normalizacion: TipoNotificacion, TipoEspacio

---
+++ SIGESAEntidades/TipoEspacio.cs
@@ -0,0 +1,14 @@
+﻿
+
+using System.Globalization;
+
+namespace SigesaEntidades
+{
+    public class TipoEspacio
+    {
+        public int IdTipoEspacio {  get; set; }
+        public string Nombre { get; set; } = null!;
+
+        public string FechaCreacion { get; set; } = null!;
+    }
+}
