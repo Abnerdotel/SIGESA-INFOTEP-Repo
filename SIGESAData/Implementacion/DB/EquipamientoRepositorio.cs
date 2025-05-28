00f8976b52af4d2bc5bb641629d072304bdb0bb2
Abner David Dotel Chalas <dotelchalas@gmail.com>
2025-05-28 05:14:10 -0400

    Cambios de DATA actualizados

--- SIGESAData/Implementacion/DB/EquipamientoRepositorio.cs
+++ SIGESAData/Implementacion/DB/EquipamientoRepositorio.cs
@@ -1,30 +1,202 @@
 ﻿
-
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Options;
+using SigesaData.Configuracion;
 using SigesaData.Contrato;
 using SigesaEntidades;
+using System.Data;
 
 namespace SigesaData.Implementacion.DB
 {
     public class EquipamientoRepositorio : IEquipamientoRepositorio
     {
-        public Task<string> Editar(Equipamiento objeto)
+        private readonly ConnectionStrings con;
+
+        public EquipamientoRepositorio(IOptions<ConnectionStrings> options)
         {
-            throw new NotImplementedException();
+            con = options.Value;
         }
 
-        public Task<int> Eliminar(int Id)
+        public async Task<List<Equipamiento>> Lista()
         {
-            throw new NotImplementedException();
+            var lista = new List<Equipamiento>();
+
+            using (var conexion = new SqlConnection(con.CadenaSQL))
+            {
+                await conexion.OpenAsync();
+                SqlCommand cmd = new SqlCommand("sp_listaEquipamiento", conexion)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                using (var dr = await cmd.ExecuteReaderAsync())
+                {
+                    while (await dr.ReadAsync())
+                    {
+                        lista.Add(new Equipamiento
+                        {
+                            IdEquipamiento = Convert.ToInt32(dr["IdEquipamiento"]),
+                            Nombre = dr["Nombre"].ToString()!,
+                            Descripcion = dr["Descripcion"].ToString()!,
+                            FechaCreacion = Convert.ToDateTime(dr["FechaCreacion"])
+                        });
+                    }
+                }
+            }
+
+            return lista;
         }
 
-        public Task<string> Guardar(Equipamiento objeto)
+        public async Task<Equipamiento?> ObtenerPorId(int id)
         {
-            throw new NotImplementedException();
+            Equipamiento? objeto = null;
+
+            using (var conexion = new SqlConnection(con.CadenaSQL))
+            {
+                await conexion.OpenAsync();
+                SqlCommand cmd = new SqlCommand("sp_obtenerEquipamientoPorId", conexion)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@IdEquipamiento", id);
+
+                using (var dr = await cmd.ExecuteReaderAsync())
+                {
+                    if (await dr.ReadAsync())
+                    {
+                        objeto = new Equipamiento
+                        {
+                            IdEquipamiento = Convert.ToInt32(dr["IdEquipamiento"]),
+                            Nombre = dr["Nombre"].ToString()!,
+                            Descripcion = dr["Descripcion"].ToString()!,
+                            FechaCreacion = Convert.ToDateTime(dr["FechaCreacion"])
+                        };
+                    }
+                }
+            }
+
+            return objeto;
         }
 
-        public Task<List<Equipamiento>> Lista()
+        public async Task<string> Guardar(Equipamiento objeto)
         {
-            throw new NotImplementedException();
+            string respuesta = "";
+
+            using (var conexion = new SqlConnection(con.CadenaSQL))
+            {
+                await conexion.OpenAsync();
+                SqlCommand cmd = new SqlCommand("sp_guardarEquipamiento", conexion)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@Nombre", objeto.Nombre);
+                cmd.Parameters.AddWithValue("@Descripcion", objeto.Descripcion);
+                cmd.Parameters.Add("@MsgError", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+
+                try
+                {
+                    await cmd.ExecuteNonQueryAsync();
+                    respuesta = Convert.ToString(cmd.Parameters["@MsgError"].Value)!;
+                }
+                catch
+                {
+                    respuesta = "Error al guardar equipamiento";
+                }
+            }
+
+            return respuesta;
+        }
+
+        public async Task<string> Editar(Equipamiento objeto)
+        {
+            string respuesta = "";
+
+            using (var conexion = new SqlConnection(con.CadenaSQL))
+            {
+                await conexion.OpenAsync();
+                SqlCommand cmd = new SqlCommand("sp_editarEquipamiento", conexion)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@IdEquipamiento", objeto.IdEquipamiento);
+                cmd.Parameters.AddWithValue("@Nombre", objeto.Nombre);
+                cmd.Parameters.AddWithValue("@Descripcion", objeto.Descripcion);
+                cmd.Parameters.Add("@MsgError", SqlDbType.VarChar, 100).Direction = ParameterDirection.Output;
+
+                try
+                {
+                    await cmd.ExecuteNonQueryAsync();
+                    respuesta = Convert.ToString(cmd.Parameters["@MsgError"].Value)!;
+                }
+                catch
+                {
+                    respuesta = "Error al editar equipamiento";
+                }
+            }
+
+            return respuesta;
+        }
+
+        public async Task<int> Eliminar(int id)
+        {
+            int respuesta = 1;
+
+            using (var conexion = new SqlConnection(con.CadenaSQL))
+            {
+                await conexion.OpenAsync();
+                SqlCommand cmd = new SqlCommand("sp_eliminarEquipamiento", conexion)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@IdEquipamiento", id);
+
+                try
+                {
+                    await cmd.ExecuteNonQueryAsync();
+                }
+                catch
+                {
+                    respuesta = 0;
+                }
+            }
+
+            return respuesta;
+        }
+
+        public async Task<List<Equipamiento>> ListarPorEspacio(int idEspacio)
+        {
+            var lista = new List<Equipamiento>();
+
+            using (var conexion = new SqlConnection(con.CadenaSQL))
+            {
+                await conexion.OpenAsync();
+                SqlCommand cmd = new SqlCommand("sp_listaEquipamientoPorEspacio", conexion)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+
+                cmd.Parameters.AddWithValue("@IdEspacio", idEspacio);
+
+                using (var dr = await cmd.ExecuteReaderAsync())
+                {
+                    while (await dr.ReadAsync())
+                    {
+                        lista.Add(new Equipamiento
+                        {
+                            IdEquipamiento = Convert.ToInt32(dr["IdEquipamiento"]),
+                            Nombre = dr["Nombre"].ToString()!,
+                            Descripcion = dr["Descripcion"].ToString()!,
+                            FechaCreacion = Convert.ToDateTime(dr["FechaCreacion"])
+                        });
+                    }
+                }
+            }
+
+            return lista;
         }
     }
 }
--- SIGESAEntidades/Usuario.cs
+++ SIGESAEntidades/Usuario.cs
@@ -1,16 +1,16 @@
 ﻿namespace SigesaEntidades
 {
-    public class Usuario
-    {
-        public int IdUsuario { get; set; }
-        public string NumeroDocumentoIdentidad { get; set; } = null!;
-        public string Nombre { get; set; } = null!;
-        public string Apellido { get; set; } = null!;
-        public string Correo { get; set; } = null!;
-        public string Clave { get; set; } = null!;
-        public RolUsuario RolUsuario { get; set; } = null!;
-        public DateTime FechaCreacion { get; set; } 
-        public ICollection<Reserva> Reservas { get; set; } = new List<Reserva>();
-        public ICollection<Notificacion> Notificaciones { get; set; } = new List<Notificacion>();
-    }
+        public class Usuario
+        {
+            public int IdUsuario { get; set; }
+            public string NumeroDocumentoIdentidad { get; set; } = null!;
+            public string Nombre { get; set; } = null!;
+            public string Apellido { get; set; } = null!;
+            public string Correo { get; set; } = null!;
+            public string Clave { get; set; } = null!;
+            public RolUsuario RolUsuario { get; set; } = null!;
+            public DateTime FechaCreacion { get; set; } 
+            public ICollection<Reserva> Reservas { get; set; } = new List<Reserva>();
+            public ICollection<Notificacion> Notificaciones { get; set; } = new List<Notificacion>();
+        }
 }
